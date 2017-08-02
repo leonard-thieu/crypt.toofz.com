1d33339151b2d24930fdbf8d2600e05c9276fe64
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-02 18:48:17 -0400

    Bump version.

--- toofz.NecroDancer.WebClient/Properties/AssemblyInfo.cs
+++ toofz.NecroDancer.WebClient/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("toofz.NecroDancer.WebClient")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9445f4fa-18d2-4daf-93f8-8e9967f90a51")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
