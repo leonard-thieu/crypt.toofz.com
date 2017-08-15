9c243e2595c309ec3e5be0f2bf65fe115e823204
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-15 16:40:38 -0400

    Fix tests.

--- toofz.NecroDancer.WebClient/src/app.config.ts
+++ toofz.NecroDancer.WebClient/src/app.config.ts
@@ -19,7 +19,7 @@ if (opts.apiBaseUrl.endsWith('/')) {
 
 angular
     .module('necrodancer.app')
-    .constant('apiBaseUrl', undefined)
+    .constant('apiBaseUrl', opts.apiBaseUrl)
     .config((cfpLoadingBarProvider: angular.loadingBar.ILoadingBarProvider) => {
         'ngInject';
         cfpLoadingBarProvider.latencyThreshold = 0;
