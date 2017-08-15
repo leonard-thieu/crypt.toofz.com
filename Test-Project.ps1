0aed82bc70c8fdc836f9c8ef45c4744f04b4ebb8
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-15 14:41:26 -0400

    Run tests for each reporter.

--- Test-Project.ps1
+++ Test-Project.ps1
@@ -1,9 +1,12 @@
 cd $env:PROJECT
 
 $env:PATH += ';' + (Resolve-Path .\node_modules\.bin).Path
+# Tests are run twice, once with an AppVeyor reporter and then with the spec reporter.
+# A multiplexing reporter would be ideal here but current solutions don't seem to handle output well.
 electron-mocha --opts .\tests\mocha-appveyor.opts
-# Still running into the cleanup issue.
-# if ($LASTEXITCODE -ne 0) { $Host.SetShouldExit($LASTEXITCODE) }
+if ($LASTEXITCODE -ne 0) { $Host.SetShouldExit($LASTEXITCODE) }
+electron-mocha --opts .\tests\mocha.opts
+if ($LASTEXITCODE -ne 0) { $Host.SetShouldExit($LASTEXITCODE) }
 remap-istanbul --input .\coverage\coverage-final.json --output .\coverage\coverage-final.json
 if ($LASTEXITCODE -ne 0) { $Host.SetShouldExit($LASTEXITCODE) }
 istanbul report --report lcovonly
