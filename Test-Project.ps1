ef2b74c9bd47b5cda152be66972d3ad3aac335b0
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-15 13:39:29 -0400

    Fix paths.

--- Test-Project.ps1
+++ Test-Project.ps1
@@ -3,7 +3,8 @@ cd $env:PROJECT
 $env:PATH += ';' + (Resolve-Path .\node_modules\.bin).Path
 electron-mocha --opts .\tests\mocha-appveyor.opts
 if ($LASTEXITCODE -ne 0) { $Host.SetShouldExit($LASTEXITCODE) }
-remap-istanbul --input .\coverage\coverage-final.json --output .\coverage\coverage-final.json
+$coveragePath = (Resolve-Path .\coverage\coverage-final.json).Path
+remap-istanbul --input "$coveragePath" --output "$coveragePath"
 if ($LASTEXITCODE -ne 0) { $Host.SetShouldExit($LASTEXITCODE) }
 istanbul report --report lcovonly
 if ($LASTEXITCODE -ne 0) { $Host.SetShouldExit($LASTEXITCODE) }
