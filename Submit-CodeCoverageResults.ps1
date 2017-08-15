d94f41e2d9262d32294f52b95c7724e21f4b36b2
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-15 08:20:10 -0400

    Wording.

--- Submit-CodeCoverageResults.ps1
+++ Submit-CodeCoverageResults.ps1
@@ -3,18 +3,18 @@ cd $env:PROJECT
 if (Test-Path Env:\APPVEYOR_PULL_REQUEST_NUMBER) {
     $msg = 'The environment variable "COVERALLS_REPO_TOKEN" is a secure environment variable. ' + `
            'Secure environment variables are not available during pull request builds. ' + `
-           'Code coverage results have not been uploaded.'
+           'Code coverage results have not been submitted.'
     Write-Warning $msg
 } elseif (!(Test-Path Env:\COVERALLS_REPO_TOKEN)) {
     $msg = 'The environment variable "COVERALLS_REPO_TOKEN" is not set. ' + `
-           'Code coverage results have not been uploaded.'
+           'Code coverage results have not been submitted.'
     Write-Warning $msg
 } else {
     $env:PATH = $env:PATH + ';' + (Resolve-Path ./node_modules/.bin).Path
     Get-Content ./coverage/lcov.info | coveralls
     if ($LASTEXITCODE -ne 0) { $Host.SetShouldExit($LASTEXITCODE) }
 
-    Write-Output 'Code coverage results have been uploaded.'
+    Write-Output 'Code coverage results have been submitted.'
 }
 
 cd ..
