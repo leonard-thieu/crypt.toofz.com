7a41ec33ea830cba94720f3c2f0e79d3e8caf323
Leonard Thieu <leonard.thieu@gmail.com>
2017-10-16 15:17:40 -0400

    Enable caching and cache busting.

---
+++ toofz.NecroDancer.WebClient/FingerPrint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Hosting;
+
+namespace toofz.NecroDancer.WebClient
+{
+    // https://madskristensen.net/post/cache-busting-in-aspnet
+    public static class Fingerprint
+    {
+        public static string Tag(string rootRelativePath)
+        {
+            if (HttpRuntime.Cache[rootRelativePath] == null)
+            {
+                string absolute = HostingEnvironment.MapPath("~" + rootRelativePath);
+
+                DateTime date = File.GetLastWriteTime(absolute);
+                int index = rootRelativePath.LastIndexOf('/');
+
+                string result = rootRelativePath.Insert(index, "/v-" + date.Ticks);
+                HttpRuntime.Cache.Insert(rootRelativePath, result, new CacheDependency(absolute));
+            }
+
+            return HttpRuntime.Cache[rootRelativePath] as string;
+        }
+    }
+}
