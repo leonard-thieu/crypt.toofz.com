55b73cfc413891b6f681bffecb59a7d579a1d6ae
Leonard Thieu <leonard.thieu@gmail.com>
2017-10-16 15:25:09 -0400

    Fix AppVeyor build.

--- Test-Project.ps1
+++ Test-Project.ps1
@@ -1,6 +1,8 @@
 Push-Location $env:PROJECT
 
 $env:PATH += ';' + (Resolve-Path .\node_modules\.bin).Path
+gulp build:js:test
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 electron-mocha --opts .\tests\mocha-appveyor.opts
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 remap-istanbul --input .\coverage\coverage-final.json --output .\coverage\coverage-final.json
