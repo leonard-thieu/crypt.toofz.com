8084358f1b9693e89c49c1d162a5460a3660bcee
Leonard Thieu <leonard.thieu@gmail.com>
2017-11-18 16:47:30 -0500

    v2.3.1

--- toofz.NecroDancer.WebClient/Properties/AssemblyInfo.cs
+++ toofz.NecroDancer.WebClient/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyVersion("2.3.1.0")]
 
 [assembly: AssemblyCopyright("Copyright © Leonard Thieu 2017")]
 [assembly: AssemblyProduct("toofz")]
