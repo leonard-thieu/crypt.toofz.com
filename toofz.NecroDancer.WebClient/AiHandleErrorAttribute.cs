4bc9985875ce759cf20c54b3eb8381029a865cc7
Leonard Thieu <leonard.thieu@gmail.com>
2017-12-05 16:38:40 -0500

    Don't keep single type in its own namespace.

---
+++ toofz.NecroDancer.WebClient/AiHandleErrorAttribute.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Web.Mvc;
+using Microsoft.ApplicationInsights;
+
+namespace toofz.NecroDancer.WebClient
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class AiHandleErrorAttribute : HandleErrorAttribute
+    {
+        public AiHandleErrorAttribute(TelemetryClient telemetryClient)
+        {
+            this.telemetryClient = telemetryClient;
+        }
+
+        private readonly TelemetryClient telemetryClient;
+
+        public override void OnException(ExceptionContext filterContext)
+        {
+            if (filterContext != null && filterContext.HttpContext != null && filterContext.Exception != null)
+            {
+                //If customError is Off, then AI HTTPModule will report the exception
+                if (filterContext.HttpContext.IsCustomErrorEnabled)
+                {
+                    telemetryClient.TrackException(filterContext.Exception);
+                }
+            }
+            base.OnException(filterContext);
+        }
+    }
+}
--- toofz.NecroDancer.WebClient/App_Start/FilterConfig.cs
+++ toofz.NecroDancer.WebClient/App_Start/FilterConfig.cs
@@ -1,5 +1,4 @@
 ﻿using System.Web.Mvc;
-using toofz.NecroDancer.WebClient.ErrorHandler;
 
 namespace toofz.NecroDancer.WebClient
 {
