a29191667981e32d59609867e9d867da1aff4a73
Leonard Thieu <leonard.thieu@gmail.com>
2018-01-13 19:37:05 -0500

    Port to ASP.NET Core.

---
+++ src/toofz.NecroDancer.Web/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using toofz.NecroDancer.Web.Models;
+
+namespace toofz.NecroDancer.Web.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/toofz.NecroDancer.Web/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace toofz.NecroDancer.Web
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly string Version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
